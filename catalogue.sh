a9faae1c56cc8aa334428f3b5ad7c337dd2edc73
sai-chandana-ravuri <saichandanaravuri@gmail.com>
2026-04-18 19:15:05 +0530

    code changes

--- catalogue.sh
+++ catalogue.sh
@@ -4,7 +4,7 @@ USER_ID=$(id -u)
 LOGS_FOLDER="/var/log/shell-roboshop"
 LOGS_FILE="/var/log/shell-roboshop/$0.log"
 SCRIPT_DIR=$PWD
-MONGODB_HOST=mongodb.daws88s.online
+MONGODB_HOST=mongodb.daws88c.online
 
 R='\e[31m'
 G='\e[32m'
@@ -13,19 +13,19 @@ B='\e[34m'
 N='\e[0m'
 
 if [ $USER_ID -ne 0 ]; then
-   echo -e "$R Please use admin access to install.. $N" | tee -a $LOGS_FILE
+   echo -e "$RPlease use admin access to install..$N" | tee -a $LOGS_FILE
    exit 1
 else
-   echo -e "$G Proceeding with installation.. $N"
+   echo -e "$GProceeding with installation..$N"
 fi
 
 mkdir -p $LOGS_FOLDER
 
 VALIDATE(){
 if [ $1 -eq 0 ]; then
-   echo -e "$2...$G SUCCESS $N" | tee -a $LOGS_FILE
+   echo -e "$2...$GSUCCESS$N" | tee -a $LOGS_FILE
 else
-   echo -e "$2...$R FAILURE $N" | tee -a $LOGS_FILE
+   echo -e "$2...$RFAILURE$N" | tee -a $LOGS_FILE
    exit 1
 fi
 }
@@ -44,7 +44,7 @@ if [ $? -ne 0 ]; then
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOGS_FILE
     VALIDATE $? "Creating system user"
 else
-    echo "Roboshop user already existed..$Y SKIPPING $N"
+    echo "Roboshop user already existed.. $YSKIPPING$N"
 fi
 
 mkdir -p /app 
