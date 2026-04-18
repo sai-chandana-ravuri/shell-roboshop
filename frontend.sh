79ffb66bec6b6bedfad707a5b48db76575a53b9f
sai-chandana-ravuri <saichandanaravuri@gmail.com>
2026-04-18 23:14:46 +0530

    code changes

---
+++ frontend.sh
@@ -0,0 +1,59 @@
+ #!/bin/bash
+
+USER_ID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="/var/log/shell-roboshop/$0.log"
+SCRIPT_DIR=$PWD
+MONGODB_HOST=mongodb.daws88c.online
+MYSQL_HOST=mysql.daws88c.online
+
+R='\e[31m'
+G='\e[32m'
+Y='\e[33m'
+B='\e[34m'
+N='\e[0m'
+
+if [ $USER_ID -ne 0 ]; then
+   echo -e "$R Please use admin access to install.. $N" | tee -a $LOGS_FILE
+   exit 1
+else
+   echo -e "$G Proceeding with installation.. $N"
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+if [ $1 -eq 0 ]; then
+   echo -e "$2...$G SUCCESS $N" | tee -a $LOGS_FILE
+else
+   echo -e "$2...$R FAILURE $N" | tee -a $LOGS_FILE
+   exit 1
+fi
+}
+
+dnf module disable nginx -y
+dnf module enable nginx:1.24 -y
+dnf install nginx -y
+VALIDATE $? "Enable and installing nginx"
+
+systemctl enable nginx 
+systemctl start nginx 
+VALIDATE $? "Enable and start nginx"
+
+rm -rf /usr/share/nginx/html/* 
+VALIDATE $? "Removing default content"
+
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+VALIDATE $? "Downlaoding code"
+
+cd /usr/share/nginx/html 
+unzip /tmp/frontend.zip
+VALIDATE $? "Unzipping code"
+
+rm -rf /etc/nginx/nginx.conf
+
+cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
+VALIDATE $? "Replacing nginx content"
+
+systemctl restart nginx 
+VALIDATE $? "Restarted nginx"
