92563c8f94e253ad2f51ee0b1691aabab5b1473a
sai-chandana-ravuri <saichandanaravuri@gmail.com>
2026-04-18 18:25:10 +0530

    code changes

---
+++ mongodb.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+USER_ID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="/var/log/shell-roboshop/$0.log"
+
+R='\e[31m'
+G='\e[32m'
+Y='\e[33m'
+B='\e[34m'
+N='\e[0m'
+
+if [ $USER_ID -ne 0 ]; then
+   echo -e "$R Please use admin access to install..$N" | tee -a $LOGS_FILE
+   exit 1
+else
+   echo -e "$G Proceeding with installation.."
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+if [ $1 -eq 0 ]; then
+   echo -e "$2...$G SUCCESS" | tee -a $LOGS_FILE
+else
+   echo -e "$2...$R FAILURE" | tee -a $LOGS_FILE
+   exit 1
+fi
+}
+
+cp mongo.repo /etc/yum.repos.d &>>$LOGS_FILE
+VALIDATE $? "Copying Mongo Repo.."
+
+
+dnf install mongodb-org -y &>>$LOGS_FILE
+VALIDATE $? "Installing Mongodb Server.."
+
+systemctl enable mongod 
+VALIDATE $? "Enabling Mongodb Server.."
+
+systemctl start mongod
+VALIDATE $? "Starting Mongodb Server.."
+
+sed 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf 
+VALIDATE $? "Allowing connections from internet.."
+
+systemctl restart mongod
+VALIDATE $? "Restarting Mongodb Server.."
